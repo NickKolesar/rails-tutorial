15efb2e3ec6f71af6cacf797380432731da5670e
NickKolesar <Nicholas.Mark.Kolesar@Gamil.com>
2016-07-27 02:24:51 +0000

    Finish user edit, update, index, and destory actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobarbaz",
+             password_confirmation: "foobarbaz",
+             admin: true)
+
+User.create!(name:  "Percey Fredrickstein Von Musel Klossowski de Rolo III",
+             email: "nomercypercy@voxmachina.td",
+             password:              "diplomacy",
+             password_confirmation: "diplomacy",
+             admin: true)
+             
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
