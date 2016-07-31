2d471f000d015bd10bbc8a0570e3d4606f1a113a
NickKolesar <Nicholas.Mark.Kolesar@Gamil.com>
2016-07-31 20:53:27 +0000

    issue with test config host

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: 'http://rails-tutorial-nicholasmark.c9users.io' }
+  config.action_mailer.default_url_options = { host: 'rails-tutorial-nicholasmark.c9users.io' }
 
 
   # Print deprecation notices to the stderr.
