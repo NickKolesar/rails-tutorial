c4cd2dd6b38a6d102101117185c61b091b608c6b
NickKolesar <Nicholas.Mark.Kolesar@Gamil.com>
2016-08-05 05:50:05 +0000

    Changed db seed for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "exSampleApp34",
-             password_confirmation: "exSampleApp34",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
+#User.create!(name:  "Example User",
+#             email: "example@railstutorial.org",
+#             password:              "foobarbaz",
+#             password_confirmation: "foobarbaz",
+#             admin: true,
+#             activated: true,
+#             activated_at: Time.zone.now)
 
 
-User.create!(name:  "Percey Fredrickstein Von Musel Klossowski de Rolo III",
-             email: "nomercypercy@voxmachina.td",
-             password:              "diplomacy",
-             password_confirmation: "diplomacy",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
+#User.create!(name:  "Percey Fredrickstein Von Musel Klossowski de Rolo III",
+#             email: "nomercypercy@voxmachina.td",
+#             password:              "diplomacy",
+#             password_confirmation: "diplomacy",
+#             admin: true,
+#             activated: true,
+#             activated_at: Time.zone.now)
 
              
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
-  password = "fakerPasswordSampleApp"
+  password = SecureRandom.urlsafe_base64(8)
   User.create!(name:  name,
                email: email,
                password:              password,
